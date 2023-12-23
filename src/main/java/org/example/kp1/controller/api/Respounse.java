cee42ff2c480c8ed2272dbf0cf194ac9f7ffb727
admin <dimkageo1@gmail.com>
2023-12-23 04:52:53 +0200

    Додана переодресація

--- src/main/java/org/example/kp1/controller/api/Respounse.java
+++
@@ -1,4 +0,0 @@
-package org.example.kp1.controller.api;
-
-public class Respounse {
-}
--- src/main/java/org/example/kp1/users/UserRepositori.java
+++
@@ -1,9 +0,0 @@
-package org.example.kp1.users;
-
-import org.example.kp1.users.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepositori extends JpaRepository<User, Long> {
-}
